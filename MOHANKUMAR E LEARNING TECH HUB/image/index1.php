33d132d9b8c708a1a55750639ada84369b6b7a38
Crazylove23 <144906581+Crazylove23@users.noreply.github.com>
2024-03-15 16:23:20 +0530

    Add files via upload

---
+++ MOHANKUMAR E LEARNING TECH HUB/image/index1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the home page content.</p>
+    <a href="register1.php">Register</a>
+    <a href="login1.php">Login</a>
+</body>
+</html>
